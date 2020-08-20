7c53dc8a7643ae479ec1201d74e44003b157aadf
samaswin87 <aswin.r312@outlook.com>
2020-08-20 18:00:16 +0530

    fixed the smtp settings with more option

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => ENV['HOST'] }
+  config.action_mailer.default_url_options = { :host => ENV['EMAIL_HOST'] }
   config.action_mailer.perform_deliveries = true
 
   # SMTP setting
