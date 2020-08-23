4373f27b84bae5e2ee7e43d6d989ce4b15ea3b90
samaswin87 <aswin.r312@outlook.com>
2020-08-23 23:04:10 +0530

    fixed the style and fixed the nav gem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,7 +25,6 @@
 //= require_tree .
 
 $(document).ready(function(){
-  $('.sidebar-menu > li.treeview > a').append("<span class='pull-right-container'><i class='fa fa-angle-left pull-right'></i></span>")
   var clipboard;
   if (typeof ClipboardJS !== 'undefined')
     clipboard = new ClipboardJS('.clipboard-btn');
