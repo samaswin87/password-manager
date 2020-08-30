20ae2ffd48d2d7d62f9204b0d3b2054cbfcfd42a
samaswin87 <aswin.r312@outlook.com>
2020-08-30 22:49:10 +0530

    fixed the user import

--- app/models/gender.rb
+++ app/models/gender.rb
@@ -7,16 +7,4 @@
 #  updated_at | timestamp without time zone |           | not null |
 class Gender < ApplicationRecord
 
-  def self.find_gender(value)
-    if %w(Male Female male female).include?(value)
-      Gender.find_by(alias: value.downcase)
-    elsif %w(m f M F).include?(value)
-      gender_name = (value.downcase == 'm') ? 'Male' : 'Female'
-      Gender.find_by(alias: gender_name)
-    elsif value
-      Gender.find_by(alias: 'male')
-    else
-      Gender.find_by(alias: 'female')
-    end
-  end
 end
--- app/services/user_import_service.rb
+++ app/services/user_import_service.rb
@@ -9,18 +9,24 @@ class UserImportService < ApplicationService
     import = FileImport.find(@import_id)
     csv_processed = SmarterCSV.process(import.data.path, options)
     users = []
+    user_type = UserType.user
     csv_processed.each do |batch|
       batch.each do |row|
         users << {
           first_name: row[field_mapper[:first_name]],
           last_name: row[field_mapper[:last_name]],
-          gender: Gender.find_gender(row[field_mapper[:gender]]),
+          gender_id: Gender.find_gender(row[field_mapper[:gender]]).id,
           email: row[field_mapper[:email]],
+          user_type_id: user_type.id
         }
       end
     end
-    User.import(users, on_duplicate_key_update: {conflict_target: [:email], columns: [:first_name, :last_name, :gender, :email]},
-        batch_size: 100)
+    begin
+      User.import(users, on_duplicate_key_update: {conflict_target: [:email], columns: [:first_name, :last_name, :gender_id, :email]},
+          batch_size: 100, raise_error: true )
+    rescue StandardError => e
+      puts "Errors: #{e}"
+    end
   end
 
   private
@@ -38,4 +44,19 @@ class UserImportService < ApplicationService
       email: @field_maps['email'].to_symbol
     }
   end
+
+  def find_gender(value)
+    @genders_map ||= Gender.all.group_by(&:alias)
+    if %w(Male Female male female).include?(value)
+      @genders_map[value.downcase].first
+    elsif %w(m f M F).include?(value)
+      gender_name = (value.downcase == 'm') ? 'Male' : 'Female'
+      @genders_map[gender_name.downcase].first
+    elsif value
+      @genders_map['male'].first
+    else
+      @genders_map['female'].first
+    end
+  end
+
 end
