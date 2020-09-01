8a25f736cf62c8363694f9085d42e2be2ea4b027
samaswin87 <aswin.r312@outlook.com>
2020-09-01 14:22:36 +0530

    added generator tests

--- test/lib/generators/service_generator_test.rb
+++ test/lib/generators/service_generator_test.rb
@@ -6,9 +6,23 @@ class ServiceGeneratorTest < Rails::Generators::TestCase
   destination Rails.root.join('tmp/generators')
   setup :prepare_destination
 
-  # test "generator runs without errors" do
-  #   assert_nothing_raised do
-  #     run_generator ["arguments"]
-  #   end
-  # end
+  def test_generator_runs_without_errors
+    assert_nothing_raised do
+      run_generator ["user"]
+    end
+  end
+
+  def test_generates_stub_correctly
+    run_generator ['user']
+
+    assert_file 'app/services/user_service.rb' do |service|
+      assert service.include? 'UserService < ApplicationService'
+    end
+
+    assert_file 'test/services/user_service_test.rb' do |test|
+      assert test.include? 'UserServiceTest'
+    end
+  end
+
+
 end
