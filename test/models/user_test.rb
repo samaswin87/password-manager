9158ac6e219ed21a290790256afad8eefc3fa868
samaswin87 <aswin.r312@outlook.com>
2020-09-03 10:06:37 +0530

    added user model tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,27 +3,53 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def test_validation
+    users(:john).user_type = nil
+    assert_false(users(:john).valid?)
+
+    users(:kart).email = nil
+    assert_false(users(:kart).valid?)
   end
 
   def test_associations
+    john = users(:john)
+    assert_equal(user_types(:admin), john.user_type)
+    assert_equal(genders(:male), john.gender)
+    assert_equal(addresses(:john), john.address)
+    passwords = Password.all.order(:id)
+    john.passwords.each_with_index do |address, index|
+      assert_equal(passwords[index], address)
+    end
   end
 
   def test_scopes
+    assert_equal([users(:john), users(:kart)], User.active)
+    assert_equal([users(:jack)], User.in_active)
   end
 
   def test_delegates
+    assert_equal('Male', users(:john).gender_name)
+    assert_equal('Admin', users(:john).user_type_name)
+    assert_equal('Nathalie Radial', users(:john).address_street)
+    assert_equal(5528, users(:john).address_number)
+    assert_nil(users(:john).address_additional_details)
+    assert_equal('Mckenzie Field', users(:john).address_house_name)
+    assert_equal('Chennai', users(:john).address_city_name)
+    assert_equal('Tamil Nadu', users(:john).address_state_name)
+    assert_equal('29729', users(:john).address_zipcode)
   end
 
   def test_alias
+    assert_equal('Kart Rosita', users(:kart).name)
   end
 
   def test_email
+    assert_mails(1) do
+      users(:kart).send_mail
+    end
   end
 
   def test_full_name
-  end
-
-  def test_invite
+    assert_equal('Kart Rosita', users(:kart).full_name)
   end
 
   def test_status
@@ -63,6 +89,13 @@ class UserTest < ActiveSupport::TestCase
   end
 
   def test_attachment
+    john = users(:john)
+    image = File.new(File.join(Rails.root, "/test/files", "image.png"))
+    john.update_attribute(:avatar, image)
+    assert_not_nil(john.reload.avatar_updated_at)
+    assert_equal('image.png', john.avatar_file_name)
+    assert_equal('image/png', john.avatar_content_type)
+    assert_equal(16113, john.avatar_file_size)
   end
 
   private
