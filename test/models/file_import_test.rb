2c00eee0e725596f09d636c3d8c2bfb6deac5dfe
samaswin87 <aswin.r312@outlook.com>
2020-09-03 11:12:26 +0530

    added file import initial tests

--- test/models/file_import_test.rb
+++ test/models/file_import_test.rb
@@ -2,10 +2,17 @@ require 'test_helper'
 
 class FileImportTest < ActiveSupport::TestCase
 
-  def test_validation
+  def setup
+    file_without_error = File.new(File.join(Rails.root, "/test/files", "usercsv.csv"))
+    file_with_error = File.new(File.join(Rails.root, "/test/files", "usercsv-error.csv"))
+    @file1 = file_imports(:file1)
+    @file1.update_attribute(:data, file_without_error)
+    @file2 = file_imports(:file2)
+    @file2.update_attribute(:data, file_with_error)
   end
 
   def test_associations
+    assert_equal(users(:john) ,file_imports(:file1).source)
   end
 
   def test_create
@@ -17,12 +24,4 @@ class FileImportTest < ActiveSupport::TestCase
   def test_delete
   end
 
-  def test_attachment
-  end
-
-  def test_state
-  end
-
-  def test_count
-  end
 end
