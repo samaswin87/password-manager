1316ad41892690919978776d858a8077aba158b0
samaswin87 <aswin.r312@outlook.com>
2020-09-05 23:29:39 +0530

    added more tests

--- test/controllers/passwords_controller_test.rb
+++ test/controllers/passwords_controller_test.rb
@@ -9,4 +9,30 @@ class PasswordsControllerTest < ActionController::TestCase
     assert_response(:success)
     assert_not_nil assigns(:passwords)
   end
+
+  def test_create_with_error
+    sign_in(users(:john))
+    params = password_params
+    params[:password][:url] = nil
+    post(:create, params: params)
+    assert_equal('Url cannot be blank', flash[:alert])
+    assert_template :new
+  end
+
+  private
+
+  def password_params
+    {password: {
+      name: "Password",
+      url: "http://test.com",
+      username: "test",
+      text_password: "test",
+      key: "test",
+      ssh_private_key: "--key--",
+      details: "test",
+      user_id: users(:john).id,
+      ssh_public_key: "--key--"
+    }}
+  end
+
 end
