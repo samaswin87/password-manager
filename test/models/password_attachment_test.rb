8b048fafc6352d67c27ae1f4c50198efee73a496
samaswin87 <aswin.r312@outlook.com>
2020-09-12 07:35:36 +0530

    added password attachment tests

--- test/models/password_attachment_test.rb
+++ test/models/password_attachment_test.rb
@@ -4,12 +4,31 @@ class PasswordAttachmentTest < ActiveSupport::TestCase
 
   def setup
     @file = File.new(File.join(Rails.root, "/test/files", "user.csv"))
-    @file1 = File.new(File.join(Rails.root, "/test/files", "user-error.csv"))
   end
 
   def test_associations
     assert_equal(passwords(:john_facebook), password_attachments(:attachment_1).password)
   end
 
+  def test_create
+    assert_difference("PasswordAttachment.count", 1) do
+      PasswordAttachment.create(attachment_params)
+    end
+  end
+
+  def test_destroy
+    assert_difference("PasswordAttachment.count", -1) do
+      PasswordAttachment.last.destroy
+    end
+  end
+
+  private
+
+  def attachment_params
+    {
+      attachment: @file,
+      password: passwords(:john_facebook)
+    }
+  end
 
 end
