5f94b00d41474dccaf8161fd5794ffba5fed18ec
samaswin87 <aswin.r312@outlook.com>
2020-09-12 22:28:49 +0530

    fixed the locale issues

--- app/controllers/field_mappings_controller.rb
+++ app/controllers/field_mappings_controller.rb
@@ -1,2 +1,19 @@
-class FieldMappingsController < ApplicationController
+class FieldMappingsController < BaseController
+
+  # ---- breadcrumbs ----
+
+  add_breadcrumb I18n.t('field_mapping.title'), :collection_path
+
+  # ---- methods ----
+  def index
+    respond_to do |format|
+      format.html
+      format.json { render json: CityDatatable.new(params, view_context: view_context, current_user: current_user) }
+    end
+  end
+
+  def show
+    add_breadcrumb 'Show', :resource_path
+  end
+
 end
--- config/application.rb
+++ config/application.rb
@@ -18,13 +18,10 @@ module PasswordManager
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.load_path += Dir["#{Rails.root.to_s}/config/locales/**/*.{rb,yml}"]
     config.i18n.default_locale = "en"
 
     config.autoload_paths << "#{Rails.root}/lib"
-
-    I18n.config.enforce_available_locales = false
-    I18n.locale = "en"
 
     config.generators.javascript_engine = :js
     config.active_job.queue_adapter = :sidekiq
