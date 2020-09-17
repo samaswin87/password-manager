25c8e12883ab75f8908c49517a1baa895b5695d0
samaswin87 <aswin.r312@outlook.com>
2020-09-17 07:16:11 +0530

    fixed the jquery loading issue

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,15 +3,9 @@ const webpack = require('webpack')
 
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
-    $: 'jquery',
-    '$': 'jquery',
-    'window.$': 'jquery',
-    jQuery: 'jquery',
-    'jQuery': 'jquery',
-    'window.jQuery': 'jquery',
-    jquery: 'jquery',
-    'jquery': 'jquery',
-    'window.jquery': 'jquery',
+    $: 'jquery/src/jquery',
+    jquery: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
     Rails: ['@rails/ujs']
   })
 )
