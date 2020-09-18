67c5e674aa88a6fedaba8b31c685013230bc84c4
samaswin87 <aswin.r312@outlook.com>
2020-09-19 00:04:45 +0530

    added field mapping view

--- app/assets/javascripts/file_imports/show.js
+++ app/assets/javascripts/file_imports/show.js
@@ -8,16 +8,7 @@
   }
 
   $('#upload-next').click(function(event) {
-    var stepperNode = document.querySelector('#file_upload')
-    var stepper = new Stepper(document.querySelector('#file_upload'))
-
-    stepperNode.addEventListener('show.bs-stepper', function (event) {
-      console.warn('show.bs-stepper', event)
-    })
-    stepperNode.addEventListener('shown.bs-stepper', function (event) {
-      console.warn('shown.bs-stepper', event)
-    })
-    stepper.next();
+    window.location.href = url.home_path + '?page=mapper'
   });
 
   $.file_imports_show = {
