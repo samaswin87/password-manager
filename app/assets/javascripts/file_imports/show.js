6aa780cf77c141c50b0920f05adc40ee8d37b299
samaswin87 <aswin.r312@outlook.com>
2020-09-23 23:25:57 +0530

    fixed the UI for file import view

--- app/assets/javascripts/file_imports/show.js
+++ app/assets/javascripts/file_imports/show.js
@@ -19,24 +19,6 @@
     })
 
     loadDataTable()
-
-
-    const options = {}
-    const editor = new JSONEditor(container, options)
-
-    // set json
-    const initialJson = {
-        "Array": [1, 2, 3],
-        "Boolean": true,
-        "Null": null,
-        "Number": 123,
-        "Object": {"a": "b", "c": "d"},
-        "String": "Hello World"
-    }
-    editor.set(initialJson)
-
-    // get json
-    const updatedJson = editor.get()
   }
 
 
