40c95e65482dfdb8f71c70f93f7de02c48aea5e6
samaswin87 <aswin.r312@outlook.com>
2020-10-10 22:27:13 +0530

    fixed the edit link

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+
+  def href_fa_icon_with_class(icon, href)
+    content_tag(:a, href: "#{href}", class: "pr-10p") do
+      content_tag(:i, class: "fa fa-#{icon} text-white") {}
+    end
+  end
+
 end
