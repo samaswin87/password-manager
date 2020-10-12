2cc7657c320ba70e2ee228122ff8a6e2915a028d
samaswin87 <aswin.r312@outlook.com>
2020-10-12 17:50:59 +0530

    added address routes

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,6 +6,12 @@ module UsersHelper
     end
   end
 
+  def href_fa_icon_with_class_delete(icon, href)
+    content_tag(:a, href: "#{href}", class: "pr-10p", method: :delete, data: {confirm_swal: 'Are you sure?'}) do
+      content_tag(:i, class: "fa fa-#{icon} text-white") {}
+    end
+  end
+
   def submit_fa_icon_with_class(icon, href)
     content_tag(:a, href: "#{href}", class: "pr-10p", data: { 'submit-form': true }) do
       content_tag(:i, class: "fa fa-#{icon} text-white") {}
--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,9 @@ Rails.application.routes.draw do
   resources :users do
     member do
       put :status
+      resource :address, controller: :users, as: :address, only: [] do
+        delete :address_destroy, as: :destroy
+      end
     end
 
     collection do
